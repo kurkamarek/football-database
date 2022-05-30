f07ddcc40d0c4bbde29b43d5181729be630b6ca2
kurkamarek <majek.kurka@seznam.cz>
2022-05-30 18:50:24 +0200

    v2

--- football/models.py
+++ football/models.py
@@ -2,21 +2,29 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+def hrac_path(instance, filename):
+    return "hrac/" + str(instance.prijmeni) + "/fotka/" + filename
+
+
 class Soutez(models.Model):
     nazev_souteze = models.CharField(max_length=80, verbose_name="Název souteže")
 
-    pocet_tymu = models.IntegerField(validators=[MaxValueValidator(100),MinValueValidator(2)], verbose_name="Počet týmů v souteži")
+    pocet_tymu = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(2)],
+                                     verbose_name="Počet týmů v souteži")
 
     class Meta:
         ordering = ['nazev_souteze']
+        verbose_name_plural = "Soutěže"
 
     def __str__(self):
         return self.nazev_souteze
 
+
 class Klub(models.Model):
     nazev_klubu = models.CharField(max_length=70, verbose_name="Název klubu")
 
-    rok_zalozeni = models.IntegerField(validators=[MaxValueValidator(2022),MinValueValidator(1850)], verbose_name="Rok zalození klubu")
+    rok_zalozeni = models.IntegerField(validators=[MaxValueValidator(2022), MinValueValidator(1850)],
+                                       verbose_name="Rok zalození klubu")
 
     zeme = models.CharField(max_length=40, verbose_name="Země")
 
@@ -28,20 +36,25 @@ class Klub(models.Model):
 
     class Meta:
         ordering = ['nazev_klubu']
+        verbose_name_plural = "Kluby"
 
     def __str__(self):
         return self.nazev_klubu
 
-class Hrac(models.Model):
-    jmeno = models.CharField(max_length=50, verbose_name="Jméno hráče")
 
-    prijmeni = models.CharField(max_length=50, verbose_name="Příjmení hrace")
+class Hrac(models.Model):
+    jmeno = models.CharField(max_length=50, verbose_name="Jméno hráče", blank=True)
+
+    prijmeni = models.CharField(max_length=50, verbose_name="Příjmení hráče")
 
     datum_narozeni = models.DateField(verbose_name="Datum narození")
 
+    foto = models.FileField(upload_to=hrac_path, blank=True, null=True, verbose_name="Fotka hráče")
+
     klubNazev = models.ForeignKey(Klub, on_delete=models.CASCADE, verbose_name="Klub")
 
-    vyska = models.IntegerField(validators=[MaxValueValidator(300),MinValueValidator(50)], verbose_name="Výška (v cm)")
+    vyska = models.IntegerField(validators=[MaxValueValidator(300), MinValueValidator(50)], verbose_name="Výška (v cm)")
+
 
     POZICE = (
         ("utocnik", "útočník"),
@@ -61,14 +74,7 @@ class Hrac(models.Model):
 
     class Meta:
         ordering = ['prijmeni']
+        verbose_name_plural = "Hráči"
 
     def __str__(self):
-        return self.prijmeni
-
-
-
-
-
-
-
-
+        return f"{self.jmeno} {self.prijmeni}"
--- football/views.py
+++ football/views.py
@@ -1,3 +1,30 @@
+from multiprocessing import context
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from football.models import Hrac
 
-# Create your views here.
+
+def index(request):
+
+    num_hraci = Hrac.objects.all().count()
+
+    hraci = Hrac.objects.order_by('prijmeni')[:3]
+
+    context = {
+        'num_hraci': num_hraci,
+        'hraci': hraci,
+    }
+
+    return render(request, 'index.html', context=context)
+
+class HracListView(ListView):
+    model = Hrac
+
+    context_object_name = 'hraci'
+    template_name = 'list.html'
+
+class HracDetailView(DetailView):
+    model = Hrac
+
+    context_object_name = 'hraci_detail'
+    template_name = 'detail.html'
